2c35eee530b59f1b19961f3dcf7f966838a23ec4
ktaeeun43 <ktaeeun43@gmail.com>
2022-09-16 14:46:39 +0900

    포트번호 변경

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-import express from "express";
+const express = require("express");
 const app = express();
+app.set('port', process.env.PORT || 8080);
 
-app.listen(8080, () => {
-  console.log("Connected to backend.");
+app.get('/', (req, res) => {
+	res.send("Hello Exress");
+});
+
+app.listen(app.get('port'),()=>{
+	console.log(app.get('port'),"Connected.");
 });
