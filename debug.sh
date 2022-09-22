7f3116f6f2b0bff580c6c0974c3feb9c85bc78e5
Changraekang <storkcr@gmail.com>
2022-09-22 14:10:04 +0900

    SVC 포트변경

---
+++ debug.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+kubectl exec -it deployment.apps/web -n taeeun-cat-test -- /bin/sh
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-app.set("port", process.env.PORT || 8800);
+app.set("port", process.env.PORT || 80);
 
 app.get("/", (req, res) => {
   res.send("Hello Taeeun Docker~~~");
