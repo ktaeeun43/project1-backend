cb837fbff1de8042df63d760f58eeaeb8947a985
ktaeeun43 <ktaeeun43@gmail.com>
2022-09-23 13:30:21 +0900

    rds연결

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require("express");
+const mysql = require("mysql");
 const app = express();
 app.set("port", process.env.PORT || 8000);
 
+const connection = mysql.createConnection({
+  host: "test.cvzgozjbabbk.us-east-1.rds.amazonaws.com",
+  user: "root",
+  password: "root1234",
+  database: "test",
+});
+
 app.get("/", (req, res) => {
-  res.send("Hello Taeeun Docker~~~");
+  connection.connect(function (err) {
+    if (err) {
+      throw err;
+    } else {
+      connection.query("SELECT * FROM fruit", function (err, rows, fields) {
+        console.log(rows);
+        res.send(rows);
+      });
+    }
+  });
 });
 
 app.listen(app.get("port"), () => {
